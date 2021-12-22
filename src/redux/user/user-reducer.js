910c22f1043254d70b769b62fac80b0c5fa7754c
0xLil14n <0xLil14n@gmail.com>
2021-12-21 19:28:24 -0600

    redirect signed in user to homepage

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,23 +1,24 @@
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
-    currentUser: null
+  currentUser: null,
 };
 const userReducer = (state = INITIAL_STATE, action) => {
-    // if state is null, it WON'T take the initial state bc null is considered a real value
-    // reducer == fn that takes in CURRENT STATE and ACTION
-    // receives STATE which is the original state/initial state 
-    // ACTION
-    // actions have a 1) type, 2) payload
-    // every reducer gets every single action that gets fired
-    switch(action.type) {
-        case 'SET_CURRENT_USER':
-            return {
-                ...state,
-                currentUser: action.payload
-            };
-        default:
-            return state;
-
-    }
+  // if state is null, it WON'T take the initial state bc null is considered a real value
+  // reducer == fn that takes in CURRENT STATE and ACTION
+  // receives STATE which is the original state/initial state
+  // ACTION
+  // actions have a 1) type, 2) payload
+  // every reducer gets every single action that gets fired
+  switch (action.type) {
+    case UserActionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
